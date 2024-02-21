ea1e18b31314d69a815733a8d589c8af850ea50e
= <=>
2024-02-21 14:55:14 +0200

    .

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,6 +1,4 @@
 import axios from "axios";
 
-console.log(process.env.REACT_APP_SERVER_URL);
-
-axios.BASE_URL = process.env.REACT_APP_SERVER_URL;
+axios.BASE_URL = process.env.REACT_APP_SERVER_URL || "";
 axios.defaults.withCredentials = true;
